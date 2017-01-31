9527ecfb87495b8a21c95cf3c76b557f2773dfb5
Ricardo Lopez <r-lopez@kellogg.northwestern.edu>
2017-01-30 20:15:22 -0600

    1st Push

---
+++ todo/todo.js
@@ -0,0 +1,11 @@
+// This code runs when the page loads
+$(function() {
+
+  $(".thumbnail").on("click", function(event) {
+    event.preventDefault()
+    var elementThatWasClicked = $(this)
+    elementThatWasClicked.parent().remove()
+    // remove the element
+  })
+
+})
